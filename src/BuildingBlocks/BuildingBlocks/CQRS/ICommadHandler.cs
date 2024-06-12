eabc9b37c1fd1adf5fe84fc251c8645ddc65fc2b
Ahmed Elhawary <Ahmed.Elhawary@integrant.com>
2024-06-12 10:56:19 +0300

    section 49

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommadHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface ICommandHandler<in TCommand> : ICommadHandler<TCommand,Unit> where TCommand : ICommand<Unit>
+    {
+    }
+    public interface ICommadHandler<in TCommand,Tresponse> : IRequestHandler<TCommand, Tresponse> where TCommand : ICommand<Tresponse> where Tresponse : notnull
+    {
+    }
+}
--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,16 +1,27 @@
-﻿using MediatR;
-
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 namespace Catalog.API.Products.CreateProduct
 {
-    public record CreateProductCommand(string Name, string Description, decimal Price, string ImageFile, List<string> Category) :IRequest<CreateProductResult>;
+    public record CreateProductCommand(string Name, string Description, decimal Price, string ImageFile, List<string> Category) :ICommand<CreateProductResult>;
 
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler : ICommadHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+          var product = new Product
+          {
+
+              Name = command.Name, 
+              Description = command.Description, 
+              Price = command.Price, 
+              ImageFile = command.ImageFile, 
+              Category = command.Category 
+          };
+
+            return new CreateProductResult(Guid.NewGuid());
+           //throw new NotImplementedException();
         }
     }
 }
