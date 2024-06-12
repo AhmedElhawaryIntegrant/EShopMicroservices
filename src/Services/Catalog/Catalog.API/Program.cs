27b3d748be0d8ddfc697c1fae4eca08c191844f9
Ahmed Elhawary <Ahmed.Elhawary@integrant.com>
2024-06-12 13:41:57 +0300

    commit section 5

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCarter();
+builder.Services.AddMediatR(configuration =>
+{
+    configuration.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 var app = builder.Build();
+app.MapCarter();
 
-app.MapGet("/", () => "Hello World!");
+
 
 app.Run();
