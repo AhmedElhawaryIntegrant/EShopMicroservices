38df7b131e78e0e4121d75fddb0fd8aaa53f23f6
Ahmed Elhawary <Ahmed.Elhawary@integrant.com>
2024-06-20 06:31:47 +0300

    Add Pagination

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,15 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using Marten.Pagination;
+using System.Collections.Generic;
 
 namespace Catalog.API.Products.GetProducts
 {
-    public record GetProductsQuery():IQuery<GetProductsResult>;
+    public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) :IQuery<GetProductsResult>;
     public record GetProductsResult(IEnumerable<Product> Products);
     public class GetProductsHandler(IDocumentSession session,ILogger<GetProductsHandler> logger) : IQueryHandler<GetProductsQuery, GetProductsResult>
     {
         public async Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
         {
             logger.LogInformation("GetProductsHandler.Handle");
-            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            // var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            var products = await session.Query<Product>().ToPagedListAsync(request.PageNumber ?? 1, request.PageSize ?? 10, cancellationToken);
             return new GetProductsResult(products);
         }
     }
