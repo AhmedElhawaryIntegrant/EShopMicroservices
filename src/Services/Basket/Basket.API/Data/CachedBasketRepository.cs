95245bcad1fa129f84bd17025a5f9f778412e83b
Ahmed Elhawary <Ahmed.Elhawary@integrant.com>
2024-06-24 01:19:20 +0300

    Add redis cach

---
+++ src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,39 @@
+﻿
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Data
+{
+    public class CachedBasketRepository(IBasketRepository repository,IDistributedCache distributedCach) : IBasketRepository
+    {
+        public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+        {
+             await repository.DeleteBasket(userName, cancellationToken);
+            await distributedCach.RemoveAsync(userName, cancellationToken);
+            return true;
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            var cachedBasket = await distributedCach.GetStringAsync(userName, cancellationToken);
+            if(!string.IsNullOrEmpty(cachedBasket))
+            {
+                return JsonSerializer.Deserialize<ShoppingCart>(cachedBasket);
+            }
+            else
+            {
+                var basket = await repository.GetBasket(userName, cancellationToken);
+                await distributedCach.SetStringAsync(userName, JsonSerializer.Serialize(basket), cancellationToken);
+                return basket;
+            }
+        
+        }
+
+        public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+        {
+            var baske =  await repository.StoreBasket(basket, cancellationToken);
+            await distributedCach.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket), cancellationToken);
+            return basket;
+        }
+    }
+}
