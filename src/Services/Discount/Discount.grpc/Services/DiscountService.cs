84acca502d554e5a6f24a60d9cc51945f53c9eb1
Ahmed Elhawary <Ahmed.Elhawary@integrant.com>
2024-06-25 16:21:38 +0300

    Add discount service

---
+++ src/Services/Discount/Discount.grpc/Services/DiscountService.cs
@@ -0,0 +1,30 @@
+﻿using Discount.Grpc;
+using Grpc.Core;
+
+namespace Discount.grpc.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        public override Task<CoupnModel> GetDiscount(DiscountRequest request, ServerCallContext context)
+        {
+            return base.GetDiscount(request, context);
+        }
+
+        public override Task<CoupnModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            return base.CreateDiscount(request, context);
+        }
+
+        public override Task<CoupnModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            return base.UpdateDiscount(request, context);
+        }
+
+        public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            return base.DeleteDiscount(request, context);
+        }
+
+
+    }
+}
