6dc500ab53f4b333762673134f7f87675fb9d01d
Ahmed Elhawary <Ahmed.Elhawary@integrant.com>
2024-07-05 09:16:54 +0300

    Add Orderind dependeny injection components

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,4 +1,12 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddApplicationService()
+    .AddInfrastructureService(builder.Configuration)
+    .AddAPIServices();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
