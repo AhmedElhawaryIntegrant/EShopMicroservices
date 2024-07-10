62b9eede0de692586cb19ac25aaa9255a885f1bc
Ahmed Elhawary <Ahmed.Elhawary@integrant.com>
2024-07-10 15:51:04 +0300

    commit ordering.api

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,7 @@
 using Ordering.API;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensionsa;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,13 @@ builder.Services.AddApplicationService()
     .AddInfrastructureService(builder.Configuration)
     .AddAPIServices();
 var app = builder.Build();
+
+app.UseAPIServices();
+
+if (app.Environment.IsDevelopment())
+{
+    await app.InitializeDatabaseAsync();
+}
 
 app.MapGet("/", () => "Hello World!");
 
