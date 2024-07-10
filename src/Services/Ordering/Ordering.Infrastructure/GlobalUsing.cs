ba0ff8aa1019800304f12efe6edf4ed845fe3399
Ahmed Elhawary <Ahmed.Elhawary@integrant.com>
2024-07-10 21:35:36 +0300

    Add Auditable Interceptor

--- src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -2,4 +2,5 @@
 global using Ordering.Domain.Enums;
 global using Ordering.Domain.ValueObject;
 global using Ordering.Domain.Models;
+global using Ordering.Domain.Abstraction;
 global using Ordering.Infrastructure.Data;
