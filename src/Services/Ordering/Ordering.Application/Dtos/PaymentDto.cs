5c6323526fbb5450a9cec6b81f0405cdb1f74a21
Ahmed Elhawary <Ahmed.Elhawary@integrant.com>
2024-07-11 14:33:44 +0300

    Add Dtos

---
+++ src/Services/Ordering/Ordering.Application/Dtos/PaymentDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Dtos
+{
+    public record class PaymentDto(string CardNumber, string CardHolderName, string Expiration, string Cvv, int PaymentMethod);
+    
+}
