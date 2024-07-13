9c9a11e7e771bc9ce4d0de45e405580870bdebfa
Ahmed Elhawary <Ahmed.Elhawary@integrant.com>
2024-07-14 00:15:34 +0300

    Add Get Order Qyery by paginated

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrderQuery/GetOrdersHandler.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Data;
+using BuildingBlocks.Pagination;
+
+namespace Ordering.Application.Orders.Queries.GetOrderQuery
+{
+    public class GetOrdersHandler(IApplicationDBContext dbContext) : IQueryHandler<GetOrdersQuery, GetOrderResult>
+    {
+        public async Task<GetOrderResult> Handle(GetOrdersQuery query, CancellationToken cancellationToken)
+        {
+            var pageSize = query.paginationRequest.PageSize;
+            var pageIndex = query.paginationRequest.PageIndex;
+            var totalCount = await dbContext.Orders.LongCountAsync(cancellationToken);
+            var orders = await dbContext.Orders
+                .Include(o => o.OrderItems)
+                .AsNoTracking()
+                .OrderBy(o => o.OrderName)
+                .Skip(pageSize * pageIndex)
+                .Take(pageSize)
+                .ToListAsync(cancellationToken);
+           return new GetOrderResult(new PaginatedResult<OrderDto>(pageIndex,pageSize,totalCount,orders.ToOrderDtoList()));
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrderQuery/GetOrdersQuery.cs
@@ -0,0 +1,10 @@
+﻿
+
+using BuildingBlocks.Pagination;
+
+namespace Ordering.Application.Orders.Queries.GetOrderQuery
+{
+    public record class GetOrderResult(PaginatedResult<OrderDto> orders);
+    public record GetOrdersQuery(PagintationRequest paginationRequest) : IQuery<GetOrderResult>;
+   
+}
