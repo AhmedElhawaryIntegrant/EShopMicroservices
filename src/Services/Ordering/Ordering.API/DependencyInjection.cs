527f3eee5501be76722583430de6e75aaacf88a7
Ahmed Elhawary <Ahmed.Elhawary@integrant.com>
2024-07-14 20:24:43 +0300

    Add create order end point

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,14 +1,17 @@
-﻿namespace Ordering.API
+﻿using Carter;
+namespace Ordering.API
 {
     public static class DependencyInjection
     {
         public static IServiceCollection AddAPIServices(this IServiceCollection services) 
         {
+            services.AddCarter();
             return services;
         }
 
         public static WebApplication UseAPIServices(this WebApplication webApplication)
         {
+            webApplication.MapCarter();
             return webApplication;
         }
     }
---
+++ src/Services/Ordering/Ordering.API/EndPoints/CreateOrder.cs
@@ -0,0 +1,25 @@
+﻿
+using Ordering.Application.Orders.Commands.CreateOrder;
+namespace Ordering.API.EndPoints
+{
+    public record CreateRecordRequest(OrderDto order);
+
+    public record CreateRecordResponse(Guid Id);
+    public class CreateOrder : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/orders", async (CreateRecordRequest request,ISender sender) =>
+            {
+               var command = request.Adapt<CreateOrderCommand>();
+              var result = await sender.Send(command);
+              var response = result.Adapt<CreateRecordResponse>();
+                return Results.Created($"/orders/{response.Id}", response);
+            }).WithName("CreateOrder")
+            .Produces<CreateRecordResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Order")
+            .WithDescription("Create Order");
+        }
+    }
+}
