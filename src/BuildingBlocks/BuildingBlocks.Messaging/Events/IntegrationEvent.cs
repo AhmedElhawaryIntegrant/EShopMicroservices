7f13629c6265292a95b7759823c5bd1efa9b8eef
Ahmed Elhawary <Ahmed.Elhawary@integrant.com>
2024-07-19 17:06:43 +0300

    Add BuilgBlock.messaging

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System.Reflection;
+
+namespace BuildingBlocks.Messaging.Events
+{
+    public record IntegrationEvent
+    {
+        public Guid Id => Guid.NewGuid();
+
+        public DateTime CreationDate => DateTime.UtcNow;
+
+        public string EventType => GetType().AssemblyQualifiedName;
+    }
+}
