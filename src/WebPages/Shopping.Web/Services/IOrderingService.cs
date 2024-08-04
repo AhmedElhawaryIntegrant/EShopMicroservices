658e07ac2a59e7273ac284a80bf72d554558f3bf
Ahmed Elhawary <Ahmed.Elhawary@integrant.com>
2024-08-04 17:17:46 +0300

    Add refit sevices and layout

---
+++ src/WebPages/Shopping.Web/Services/IOrderingService.cs
@@ -0,0 +1,16 @@
+﻿using Shopping.Web.Models.Ordering;
+
+namespace Shopping.Web.Services
+{
+    public interface IOrderingService
+    {
+        [Get("/ordering-service/orders?PageIndex={PageIndex}&PageSize={PageSize}")]
+        Task<GetOrdersResponse> GetOrders(int PageIndex,int PageSize);
+
+        [Get("/ordering-service/orders/{orderName}")]
+        Task<GetOrderByNameResponse> GetOrdersByName(string orderName);
+
+        [Get("/ordering-service/orders/customer/{customerId}")]
+        Task<GetOrderByResponse> GetOrdersByCustomer(Guid customerId);
+    }
+}
